c0123fac4911d52730ce9be6a514ba133673bc32
Sergey Simashov <simashov@gmail.com>
2021-12-06 10:36:23 +0300

    Lesson 5. Rebuilt CustomTableViewCell

--- VKClientApp/VC/Cells/CustomTableViewCell.swift
+++ VKClientApp/VC/Cells/CustomTableViewCell.swift
@@ -1,14 +1,15 @@
-//
+
 //  CustomTableViewCell.swift
 //  VKClientApp
 //
-//  Created by Sergey Simashov on 23.11.2021.
-//
+//  Created by Sergey Simashov on 06.12.2021.
+
 
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-    
+
+
     @IBOutlet weak var avatarImageView: UIImageView!
     
     @IBOutlet weak var nameLabel: UILabel!
@@ -18,28 +19,27 @@ class CustomTableViewCell: UITableViewCell {
         avatarImageView.image = nil
         nameLabel.text = nil
     }
-    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-    
+
     func configure(image: UIImage?, text: String?) {
         avatarImageView.image = image
         nameLabel.text = text
     }
-    
+
     func configure(friend: Friend) {
         if let imageName = friend.avatar {
             avatarImageView.image = UIImage(named: imageName)
         }
         nameLabel.text = friend.name
     }
-    
+
     func configure(group: Group) {
         if let imageName = group.avatar {
             avatarImageView.image = UIImage(named: imageName)
         }
         nameLabel.text = group.name
     }
-    
+
 }
